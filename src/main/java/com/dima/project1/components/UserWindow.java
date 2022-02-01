28b48a22cd6e906742b519ba0cfd94c80594060e
dullard21 <dmytro.demianenko@student.tuke.sk>
2022-02-01 23:46:28 +0300

    removed class UserWindow due to non-use in the program

--- src/main/java/com/dima/project1/components/UserWindow.java
+++
@@ -1,8 +0,0 @@
-package com.dima.project1.components;
-
-public class UserWindow extends Window{
-
-    public UserWindow(int width, int height, String title){
-        super(width, height, title);
-    }
-}
